1ffaa3e746d10b790e5c099303c7987ab0c97356
bakaikin <bakaikin@yandex.ru>
2019-03-13 14:55:42 +0300

    User list stub replaced with data obtained using Retrofit request

--- app/src/main/java/me/bkkn/users/GitHubUsersFragment.java
+++ app/src/main/java/me/bkkn/users/GitHubUsersFragment.java
@@ -9,12 +9,23 @@ import androidx.recyclerview.widget.RecyclerView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.readystatesoftware.chuck.ChuckInterceptor;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class GitHubUsersFragment extends Fragment {
 
@@ -23,6 +34,8 @@ public class GitHubUsersFragment extends Fragment {
 
     private Unbinder unbinder;
     private UserRecycleAdapter adapter;
+    private GitHubService gitHubService;
+    private Call<List<User>> call;
 
 
     @Override
@@ -36,24 +49,44 @@ public class GitHubUsersFragment extends Fragment {
         recyclerView.setAdapter(adapter);
         recyclerView.setHasFixedSize(true);
 
-        ArrayList<User> userArrayList = new ArrayList<>();
-        //()->{}
-        userArrayList.add(new User("Vasya1", "https://avatars2.githubusercontent.com/u/136?v=4"));
-        userArrayList.add(new User("Vasya2", "https://avatars2.githubusercontent.com/u/136?v=4"));
-        userArrayList.add(new User("Vasya3", "https://avatars2.githubusercontent.com/u/136?v=4"));
-        userArrayList.add(new User("Vasya4", "https://avatars2.githubusercontent.com/u/136?v=4"));
-        userArrayList.add(new User("Vasya5", "https://avatars2.githubusercontent.com/u/136?v=4"));
-        userArrayList.add(new User("Vasya6", "https://avatars2.githubusercontent.com/u/136?v=4"));
-        userArrayList.add(new User("Vasya7", "https://avatars2.githubusercontent.com/u/136?v=4"));
-        userArrayList.add(new User("Vasya8", "https://avatars2.githubusercontent.com/u/136?v=4"));
-        userArrayList.add(new User("Vasya9", "https://avatars2.githubusercontent.com/u/136?v=4"));
-        adapter.addUsers(userArrayList);
+        createRetrofitInstance();
+        call =  gitHubService.getUsers(0);
+        call.enqueue(new Callback<List<User>>() {
+            @Override
+            public void onResponse(Call<List<User>> call, Response<List<User>> response) {
+                adapter.addUsers(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<List<User>> call, Throwable t) {
+                Toast.makeText(getContext(), R.string.error_message, Toast.LENGTH_SHORT).show();
+            }
+        });
+
         return view;
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+        if(call != null){
+            call.cancel();
+        }
         unbinder.unbind();
+    }
+
+    private void createRetrofitInstance(){
+//        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+//        ChuckInterceptor chuckInterceptor = new ChuckInterceptor(getContext());
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+//                .addNetworkInterceptor(httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY))
+//                .addNetworkInterceptor(chuckInterceptor)
+                .build();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.github.com/")
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        gitHubService = retrofit.create(GitHubService.class);
     }
 }
